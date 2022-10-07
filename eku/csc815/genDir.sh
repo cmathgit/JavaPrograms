99062e956d41dd2c19138e84b0ed6ef2dcb1c1ba
U-W17FE-GIQWA\C <cruz.math@outlook.com>
2022-10-07 19:06:56 -0400

    update shell scripts

--- eku/csc815/genDir.sh
+++ eku/csc815/genDir.sh
@@ -1,5 +1,7 @@
 # shell script to generate a directory with a unique name
 # DATETIME format: YYYYMMDDHHMMSS
 # execute bash genDir.sh to run
+# touch folder$(date +"%Y%m%d%H%M%S")
+# mkdir folder$(date +"%Y%m%d%H%M%S")
 varDate=$(date +"%Y%m%d%H%M%S")
 mkdir folder$varDate
--- eku/csc815/genFile.sh
+++ eku/csc815/genFile.sh
@@ -1,5 +1,7 @@
 # shell script to generate a directory with a unique name
 # DATETIME format: YYYYMMDDHHMMSS
 # execute bash genFile.sh to run
+# touch file$(date +"%Y%m%d%H%M%S".xml)
+# touch file$(date +"%Y%m%d%H%M%S.xml")
 varDate=$(date +"%Y%m%d%H%M%S")
 touch file$varDate.txt
